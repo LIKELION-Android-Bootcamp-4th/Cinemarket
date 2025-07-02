cba0d4a6edc2e1166c9ff81e2005855703309ee9
HyunjeLee <cyeclamen07@gmail.com>
2025-07-02 22:23:52 +0900

    rename: products -> goods

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,10 +1,10 @@
 import 'package:cinemarket/features/cart/screen/cart_screen.dart';
 import 'package:cinemarket/features/favorite/screen/favorite_screen.dart';
+import 'package:cinemarket/features/goods/screen/goods_screen.dart';
 import 'package:cinemarket/features/home/screen/home_screen.dart';
 import 'package:cinemarket/features/main/screen/main_screen.dart';
 import 'package:cinemarket/features/movies/screen/movies_screen.dart';
 import 'package:cinemarket/features/mypage/screen/mypage_screen.dart';
-import 'package:cinemarket/features/products/screen/products_screen.dart';
 import 'package:cinemarket/features/search/screen/search_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -23,7 +23,7 @@ final GoRouter router = GoRouter(
         ),
         GoRoute(
           path: '/products',
-          builder: (context, state) => const ProductsScreen(),
+          builder: (context, state) => const GoodsScreen(),
         ),
         GoRoute(
           path: '/home',
--- lib/features/goods/screen/goods_screen.dart
+++ lib/features/goods/screen/goods_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProductsScreen extends StatelessWidget {
-  const ProductsScreen({super.key});
+class GoodsScreen extends StatelessWidget {
+  const GoodsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
