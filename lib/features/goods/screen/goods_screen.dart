2a64a94ae94b93199356ca22773e79cff21d5ff2
HyunjeLee <cyeclamen07@gmail.com>
2025-07-03 00:06:54 +0900

    feat/g01: 전체 굿즈 목록 UI 구현
    - 공통 위젯으로 sort_dropdown.dart 생성

--- lib/features/goods/screen/goods_screen.dart
+++ lib/features/goods/screen/goods_screen.dart
@@ -1,3 +1,7 @@
+import 'package:cinemarket/core/theme/app_colors.dart';
+import 'package:cinemarket/features/favorite/widget/favorite_gridview.dart';
+import 'package:cinemarket/features/favorite/widget/item_type.dart';
+import 'package:cinemarket/widgets/sort_dropdown.dart';
 import 'package:flutter/material.dart';
 
 class GoodsScreen extends StatelessWidget {
@@ -5,9 +9,41 @@ class GoodsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> dummyGoods = List.generate(10, (i) {
+      return {
+        'imageUrl':
+            'https://i.ebayimg.com/images/g/64YAAOSwDqhnttak/s-l1200.png',
+        'goodsName': '굿즈 ${i + 1}',
+        'movieName': '관련 영화',
+        'price': '${(i + 1) * 1000}원',
+        'rating': 4.5,
+        'reviewCount': 10,
+        'isFavorite': false,
+      };
+    });
+
     return Scaffold(
-      body: const Center(
-        child: Text('상품 목록 화면입니다'),
+      backgroundColor: AppColors.background,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 4),
+                child: SortDropdown(itemType: ItemType.goods),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: FavoriteGridview(
+                itemType: ItemType.goods,
+                items: dummyGoods,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
