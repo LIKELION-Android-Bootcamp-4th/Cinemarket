09f3b709d67798d2673494f395f3c16760814747
HyunjeLee <cyeclamen07@gmail.com>
2025-07-03 00:15:04 +0900

    feat/mv01: 전체 영화 목록 UI 구현
    - 공통 위젯으로 sort_dropdown.dart 생성

--- lib/features/movies/screen/movies_screen.dart
+++ lib/features/movies/screen/movies_screen.dart
@@ -1,3 +1,7 @@
+import 'package:cinemarket/core/theme/app_colors.dart';
+import 'package:cinemarket/features/favorite/widget/favorite_gridview.dart';
+import 'package:cinemarket/features/favorite/widget/item_type.dart';
+import 'package:cinemarket/widgets/sort_dropdown.dart';
 import 'package:flutter/material.dart';
 
 class MoviesScreen extends StatelessWidget {
@@ -5,9 +9,43 @@ class MoviesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> dummyMovies = List.generate(10, (i) {
+      return {
+        'imageUrl':
+            'https://image.tmdb.org/t/p/original/vqBmyAj0Xm9LnS1xe1MSlMAJyHq.jpg',
+        'movieName': '영화 ${i + 1}',
+        'cumulativeSales': (i + 1) * 100,
+        'providers': [
+          'https://image.tmdb.org/t/p/original/hPcjSaWfMwEqXaCMu7Fkb529Dkc.jpg',
+          'https://image.tmdb.org/t/p/original/8z7rC8uIDaTM91X0ZfkRf04ydj2.jpg',
+          'https://image.tmdb.org/t/p/original/97yvRBw1GzX7fXprcF80er19ot.jpg',
+        ],
+        'isFavorite': false,
+      };
+    });
+
     return Scaffold(
-      body: const Center(
-        child: Text('영화 목록 화면입니다'),
+      backgroundColor: AppColors.background,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 4),
+                child: SortDropdown(itemType: ItemType.movie),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: FavoriteGridview(
+                itemType: ItemType.movie,
+                items: dummyMovies,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/sort_dropdown.dart
@@ -0,0 +1,73 @@
+import 'package:cinemarket/core/theme/app_colors.dart';
+import 'package:cinemarket/core/theme/app_text_style.dart';
+import 'package:cinemarket/features/favorite/widget/item_type.dart';
+import 'package:flutter/material.dart';
+
+class SortDropdown extends StatefulWidget {
+  final ItemType itemType;
+
+  const SortDropdown({super.key, required this.itemType});
+
+  @override
+  State<SortDropdown> createState() => _SortDropdownState();
+}
+
+class _SortDropdownState extends State<SortDropdown> {
+  late List<String> sortOptions;
+  late String selected;
+
+  @override
+  void initState() {
+    super.initState();
+    sortOptions = _getSortOptions(widget.itemType);
+    selected = sortOptions.first;
+  }
+
+  List<String> _getSortOptions(ItemType itemType) {
+    switch (itemType) {
+      case ItemType.goods:
+        return ['최신순', '오래된순', '인기순'];
+      case ItemType.movie:
+        return ['최신순', '평점순', '굿즈판매량순'];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.widgetBackground,
+        borderRadius: BorderRadius.circular(8),
+      ),
+
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          padding: const EdgeInsets.only(left: 10),
+          isDense: true,
+          style: AppTextStyle.bodySmall,
+          dropdownColor: AppColors.widgetBackground,
+          value: selected,
+          icon: const Padding(
+            padding: EdgeInsets.only(left: 0),
+            child: const Icon(
+              Icons.arrow_drop_down,
+              color: AppColors.textSecondary,
+            ),
+          ),
+          items:
+          sortOptions.map((option) {
+            return DropdownMenuItem<String>(
+              value: option,
+              child: Text(option),
+            );
+          }).toList(),
+          onChanged: (value) {
+            if (value != null) {
+              setState(() => selected = value);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
