e189fee8d28555a26aa31e97183a6b034e5e2751
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-07-03 20:05:41 +0900

    [ui/MV02] 출연진 GridView UI - 더미데이터를 통한 출연진 GridView 생성

--- lib/features/movies/screen/movie_detail_screen.dart
+++ lib/features/movies/screen/movie_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemarket/features/movies/widgets/cast_grid_view.dart';
 import 'package:cinemarket/features/movies/widgets/movie_detail_header.dart';
 import 'package:cinemarket/features/movies/widgets/movie_info_row.dart';
 import 'package:cinemarket/features/movies/widgets/synopsis_text.dart';
@@ -55,7 +56,7 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                 height: screenHeight * 0.6, // 예: 전체 높이의 60%
                 child: CommonTabView(
                   tabTitles: const ['줄거리 요약', '상품', '출연진'],
-                  tabViews: [
+                  tabViews: const [
                     Padding(
                       padding: EdgeInsets.all(16),
                       child: SynopsisText(synopsis:
@@ -63,7 +64,26 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                           "최고가 되지 못한 전설 VS 최고가 되고 싶은 루키! 한때 주목받는 유망주였지만 끔찍한 사고로 F1®에서 우승하지 못하고 한순간에 추락한 드라이버 '소니 헤이스'(브래드 피트). 그의 오랜 동료인 '루벤 세르반테스'(하비에르 바르뎀)에게 레이싱 복귀를 제안받으며 최하위 팀인 APXGP에 합류한다. 그러나 팀 내 떠오르는 천재 드라이버 '조슈아 피어스'(댐슨 이드리스)와 '소니 헤이스'의 갈등은 날이 갈수록 심해지고. 설상가상 우승을 향한 APXGP 팀의 전략 또한 번번이 실패하며 최하위권을 벗어나지 못하고 고전하는데··· 빨간 불이 꺼지고 운명을 건 레이스가 시작된다!"),
                     ),
                     Center(child: Text('상품 탭', style: AppTextStyle.body)),
-                    Center(child: Text('출연진 탭', style: AppTextStyle.body)),
+                    CastGridView(
+                      castList: [
+                        {
+                          'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Dwayne_Johnson_2014_%28cropped%29.jpg',
+                          'name': 'Dwayne Johnson',
+                        },
+                        {
+                          'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/6/67/Tom_Hiddleston_%2836109110291%29_%28cropped%29.jpg',
+                          'name': 'Tom Hiddleston',
+                        },
+                        {
+                          'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/4/40/Adam_Sandler_Cannes_2017.jpg',
+                          'name': 'Adam Sandler',
+                        },
+                        {
+                          'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Jake_Gyllenhaal_2019_by_Glenn_Francis.jpg',
+                          'name': 'Chris Hemsworth',
+                        },
+                        ]
+                    )
                   ],
                 ),
               ),
---
+++ lib/features/movies/widgets/cast_item.dart
@@ -0,0 +1,42 @@
+import 'package:cinemarket/core/theme/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+class CastItem extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+
+  const CastItem({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding:EdgeInsets.symmetric(vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipOval(
+            // borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              width: 150,
+              height: 150,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            name,
+            style: AppTextStyle.section,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      )
+    );
+  }
+}
