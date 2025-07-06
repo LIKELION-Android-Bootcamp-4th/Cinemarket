b343cd14baccc41febe24f773309a8769255a16f
HyunjeLee <cyeclamen07@gmail.com>
2025-07-07 02:33:51 +0900

    feat: 각 탭바뷰 구조 구현

---
+++ lib/features/goods/widget/tabs_review.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/***
+ * 좋아요 순 또는 최신순으로 10개만 리뷰 가져오기
+ * UI는 기존 리뷰 목록과 동일하지만 하단의 좋아요/싫어요 버튼만 제거
+ */
+List<Widget> getTabsReviewWidgets() {
+  return [
+
+  ];
+}
