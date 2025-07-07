e5976081f431226f4c635eeea0951b4883e1646a
HyunjeLee <cyeclamen07@gmail.com>
2025-07-07 11:27:45 +0900

    feat: 더보기 Text에 클릭 이벤트 추가

--- lib/features/goods/widget/tabs_review.dart
+++ lib/features/goods/widget/tabs_review.dart
@@ -1,5 +1,7 @@
+import 'package:cinemarket/core/router/router.dart';
 import 'package:cinemarket/core/theme/app_text_style.dart';
-import 'package:cinemarket/features/mypage/detail/component/my_review_component.dart' show Review;
+import 'package:cinemarket/features/mypage/detail/component/my_review_component.dart'
+    show Review;
 import 'package:cinemarket/widgets/review_item.dart';
 import 'package:flutter/material.dart';
 
@@ -8,27 +10,33 @@ import 'package:flutter/material.dart';
  * UI는 기존 리뷰 목록과 동일하지만 하단의 좋아요/싫어요 버튼만 제거
  */
 List<Widget> getTabsReviewWidgets() {
-  return[
-    const Row(
+  return [
+    Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('리뷰', style: AppTextStyle.headline),
-        Spacer(),
-        Text('더보기', style: AppTextStyle.point,)
+        const Text('리뷰', style: AppTextStyle.headline),
+        const Spacer(),
+        GestureDetector(
+          onTap: () {
+            router.push('/goods/detail/review');
+          },
+          child: const Text('더보기', style: AppTextStyle.point),
+        ),
       ],
     ),
     ...List.generate(
-    10, (_) => _reviews.map(
-      (review) => ReviewItem(
-        productName: review.productName,
-        movieTitle: review.movieTitle,
-        productImage: review.productImage,
-        photoUrls: review.photoUrls,
-        initialRating: review.initialRating,
-        initialReviewText: review.initialReviewText,
+      10,
+      (_) => _reviews.map(
+        (review) => ReviewItem(
+          productName: review.productName,
+          movieTitle: review.movieTitle,
+          productImage: review.productImage,
+          photoUrls: review.photoUrls,
+          initialRating: review.initialRating,
+          initialReviewText: review.initialReviewText,
+        ),
       ),
-    ),
-  ).expand((e) => e),
+    ).expand((e) => e),
   ];
 }
 
