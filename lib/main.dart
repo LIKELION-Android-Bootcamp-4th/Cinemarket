b1d4b3fdbd9fb2f9437b5ee840766b2d469a3be1
HyunjeLee <cyeclamen07@gmail.com>
2025-07-08 15:53:23 +0900

    feat: provider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cinemarket/features/goods/viewmodel/goods_all_viewmodel.dart';
+import 'package:cinemarket/features/goods/viewmodel/goods_detail_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:cinemarket/features/home/viewModel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => HomeViewModel()),
         ChangeNotifierProvider(create: (_) => GoodsAllViewModel()),
+        ChangeNotifierProvider(create: (_) => GoodsDetailViewmodel()),
       ],
       child: MaterialApp.router(title: 'CineMarket', routerConfig: router),
     );
