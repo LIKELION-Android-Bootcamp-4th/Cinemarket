0d3d7f2201dcf89819982ec135bb195668eb7151
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-07-08 14:42:45 +0900

    [fix/MOVIE-003] movieItem 변경으로 인한 providers 렌더링 문제 해결

--- lib/features/home/widgets/best_movie_widget.dart
+++ lib/features/home/widgets/best_movie_widget.dart
@@ -12,60 +12,79 @@ class BestMovieWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final movies = [
       {
-        "id":1,
+        "id": 1,
         "imageUrl": "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240612_151%2F1718180074487NH0V5_JPEG%2Fmovie_image.jpg",
         "movieName": "인사이드 아웃 2",
         "cumulativeSales": 13874512,
         "providers": [
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Disney_plus_icon.png/960px-Disney_plus_icon.png?20231225065415"
+          {
+            "Disney+": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Disney_plus_icon.png/960px-Disney_plus_icon.png?20231225065415"
+          }
         ],
         "isFavorite": false,
       },
       {
-        "id":2,
+        "id": 2,
         "imageUrl": "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240425_256%2F17140073560223JK9r_JPEG%2Fmovie_image.jpg",
         "movieName": "범죄도시 4",
         "cumulativeSales": 9876543,
         "providers": [
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png"
+          {
+            "Netflix": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png"
+          }
         ],
         "isFavorite": true,
       },
       {
-        "id":3,
+        "id": 3,
         "imageUrl": "https://i.namu.wiki/i/BrJlUyt7V_w4jzAHrS6ksBilrTyY-wZYmD8GZCCOSy-YaH92ZIhP5l2ayOsgmamJTA7Ob18BoMYu8RmeaxMCrA.webp",
         "movieName": "퓨리오사: 매드맥스 사가",
         "cumulativeSales": 7521345,
         "providers": [
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png",
-          "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY",
+          {
+            "Netflix": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png"
+          },
+          {
+            "Watcha": "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY"
+          }
         ],
         "isFavorite": false,
       },
       {
-        "id":4,
-        "imageUrl": "https://i.namu.wiki/i/2ic9bxOdR86soB6PNyQK6sEVbvzG-5Dys9Yfqh6HnWXxsXJT2gZxaIKcyHkUR9bgnICJyxhe5F5p9eGyf7thYA.webp", // IF (2024)
+        "id": 4,
+        "imageUrl": "https://i.namu.wiki/i/2ic9bxOdR86soB6PNyQK6sEVbvzG-5Dys9Yfqh6HnWXxsXJT2gZxaIKcyHkUR9bgnICJyxhe5F5p9eGyf7thYA.webp",
         "movieName": "IF: 상상의 친구",
         "cumulativeSales": 6234987,
         "providers": [
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png",
-          "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY",
+          {
+            "Netflix": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png"
+          },
+          {
+            "Watcha": "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY"
+          }
         ],
         "isFavorite": false,
       },
       {
-        "id":5,
-        "imageUrl": "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240508_299%2F1715149331590vuPj9_JPEG%2Fmovie_image.jpg", // Kingdom of the Planet of the Apes
+        "id": 5,
+        "imageUrl": "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240508_299%2F1715149331590vuPj9_JPEG%2Fmovie_image.jpg",
         "movieName": "혹성탈출: 새로운 시대",
         "cumulativeSales": 8431123,
         "providers": [
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Disney_plus_icon.png/960px-Disney_plus_icon.png?20231225065415",
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png",
-          "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY"
+          {
+            "Disney+": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Disney_plus_icon.png/960px-Disney_plus_icon.png?20231225065415"
+          },
+          {
+            "Netflix": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/640px-Netflix_icon.svg.png"
+          },
+          {
+            "Watcha": "https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY"
+          }
         ],
         "isFavorite": true,
       },
     ];
+
 
 
     return Column(
