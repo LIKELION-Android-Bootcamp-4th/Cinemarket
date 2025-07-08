b1aeab9c5cdadb92b64b971cd5799f1a00bded6f
HyunjeLee <cyeclamen07@gmail.com>
2025-07-08 15:52:02 +0900

    feat: 더미데이터 -> 통신 응답값으로 교체

--- lib/features/goods/widget/header_goods_detail.dart
+++ lib/features/goods/widget/header_goods_detail.dart
@@ -1,9 +1,10 @@
 import 'package:cinemarket/core/theme/app_text_style.dart';
+import 'package:cinemarket/features/goods/model/goods.dart';
 import 'package:cinemarket/features/goods/widget/primary_image_widget.dart';
 import 'package:flutter/material.dart';
 
 class HeaderGoodsDetail extends StatelessWidget {
-  final Map<String, dynamic> item;
+  final Goods item;
 
   const HeaderGoodsDetail({super.key, required this.item});
 
@@ -16,8 +17,8 @@ class HeaderGoodsDetail extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             PrimaryImageWidget(
-              imageUrl: item['imageUrl'],
-              isFavorite: item['isFavorite'],
+              imageUrl: item.images.main,
+              isFavorite: item.isFavorite,
             ),
 
             const SizedBox(height: 8),
@@ -26,7 +27,7 @@ class HeaderGoodsDetail extends StatelessWidget {
               height: 60,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemCount: dummyGoodsImages.length,
+                itemCount: item.images.sub.length,
                 itemBuilder: (context, index) {
                   return Padding(
                     padding: const EdgeInsets.only(right: 8),
@@ -35,7 +36,7 @@ class HeaderGoodsDetail extends StatelessWidget {
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(16),
                         child: Image.network(
-                          dummyGoodsImages[index],
+                          item.images.sub[index],
                           fit: BoxFit.cover,
                         ),
                       ),
@@ -62,8 +63,8 @@ class HeaderGoodsDetail extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text(item['goodsName'], style: AppTextStyle.section),
-                    Text("${item['price']}", style: AppTextStyle.bodySmall),
+                    Text(item.name, style: AppTextStyle.section),
+                    Text('${item.price}', style: AppTextStyle.bodySmall),
                   ],
                 ),
               ],
@@ -74,10 +75,3 @@ class HeaderGoodsDetail extends StatelessWidget {
     );
   }
 }
-
-final List<String> dummyGoodsImages = [
-  'https://i.ebayimg.com/images/g/WgwAAOSw~qJnttY4/s-l1200.jpg',
-  'https://i.ebayimg.com/images/g/f1oAAOSwq~JnDljW/s-l1200.jpg',
-  'https://i.ebayimg.com/images/g/5RMAAOSwhlxnlJl4/s-l400.jpg',
-  'https://i.ebayimg.com/images/g/BzAAAeSweMtn22-l/s-l1200.png',
-];
