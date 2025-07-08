1cf619cad559d8f92ed425b7b41f110d2770cfb6
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-08 13:57:09 +0900

    refactor

--- lib/features/search/repository/search_repository.dart
+++ lib/features/search/repository/search_repository.dart
@@ -1,13 +1,28 @@
 import 'package:cinemarket/features/search/model/search_goods_model.dart';
+import 'package:cinemarket/features/search/model/search_tmdb_model.dart';
 import 'package:cinemarket/features/search/service/search_service.dart';
+import 'package:cinemarket/features/search/service/search_tmdb_service.dart';
 
 class SearchRepository {
   final SearchService _searchService;
 
+
   SearchRepository({SearchService? searchService})
       : _searchService = searchService ?? SearchService();
 
-  Future<List<SearchItem>> searchMovies(String keyword) async {
-    return await _searchService.fetchSearchResults(keyword);
+
+  //tmdb 영화 검색
+  Future<List<SearchTmdbModel>> searchMovies(String keyword) async {
+    return _searchService.searchMovies(keyword);
+  }
+
+  //굿즈 직접 검색
+  Future<List<SearchItem>> searchGoodsByKeyword(String keyword) {
+    return _searchService.fetchGoodsByKeyword(keyword);
+  }
+
+  //영화ID로 굿즈 검색
+  Future<List<SearchItem>> searchMovieByContentId(int contentId) {
+    return _searchService.fetchMovieByContentId(contentId);
   }
 }
