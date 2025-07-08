36e0e6c229b07a2df0d427a7cd8b773f8660297b
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-08 13:58:27 +0900

    refactor

--- lib/features/search/service/search_service.dart
+++ lib/features/search/service/search_service.dart
@@ -1,25 +1,43 @@
 import 'package:cinemarket/core/network/api_client.dart';
+import 'package:cinemarket/core/network/tmdb_client.dart';
 import 'package:cinemarket/features/search/model/search_goods_model.dart';
+import 'package:cinemarket/features/search/model/search_tmdb_model.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SearchService {
-  final Dio _dio = Dio(
-      BaseOptions(baseUrl: 'http://git.hansul.kr:3000')
-  );
+  final Dio _dio = ApiClient.dio;
+  final Dio _tmdbDio = TmdbClient.dio;
 
-  Future<List<SearchItem>> fetchSearchResults(String keyword) async {
+  //tmdb
+  Future<List<SearchTmdbModel>> searchMovies(String query) async {
+    try {
+      final apiKey = dotenv.env['TMDB_API_KEY'] ?? '';
+      final response = await _tmdbDio.get(
+        '/search/movie',
+        queryParameters: {
+          'api_key': apiKey,
+          'query': query,
+          'language': 'ko-KR',
+        },
+      );
+
+      final List results = response.data['results'];
+      return results.map((e) => SearchTmdbModel.fromJson(e)).toList();
+    } catch (e) {
+      print('tmdb 검색 오류: $e');
+      rethrow;
+    }
+  }
+
+  //굿즈 직접 검색
+  Future<List<SearchItem>> fetchGoodsByKeyword(String keyword) async {
     try {
       final response = await _dio.get(
-          '/api/products',
-          queryParameters: {
-            'search': keyword
-          },
-        options: Options(
-          headers: {
-            'X-Company-Code': '6866fd245b230f5dc709bdf3',
-            'Content-Type': 'application/json',
-          }
-        ),
+        '/api/products',
+        queryParameters: {
+          'search': keyword
+        },
       );
       final items = response.data['data']['items'] as List;
       return items.map((e) => SearchItem.fromJson(e)).toList();
@@ -30,6 +48,20 @@ class SearchService {
       } else {
         print('❌ Unknown error: $e');
       }
+      rethrow;
+    }
+  }
+
+  //영화 ID로 굿즈 검색
+  Future<List<SearchItem>> fetchMovieByContentId(int contentId) async {
+    try {
+      final response = await _dio.get(
+          '/api/content-product/products/$contentId'
+      );
+      final items = response.data['data']['items'] as List;
+      return items.map((e) => SearchItem.fromJson(e)).toList();
+    } catch (e) {
+      print('❌ 굿즈 검색 오류 (ID): $e');
       rethrow;
     }
   }
