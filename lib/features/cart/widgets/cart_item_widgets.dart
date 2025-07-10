9b55a06d33e22df2016ef0b76b4c2bd93e3a8516
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-10 15:37:42 +0900

    refactor/cart

--- lib/features/cart/widgets/cart_item_widgets.dart
+++ lib/features/cart/widgets/cart_item_widgets.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CartItem {
+  final String cartId;
+  final String productId;
   final String name;
   int quantity;
   final int price;
   bool isSelected;
+  final String? imageUrl;
 
   CartItem({
+    required this.cartId,
+    required this.productId,
     required this.name,
     required this.quantity,
     required this.price,
     this.isSelected = false,
+    this.imageUrl,
   });
 }
