bc8133ee97a78cef17fa73fa0c6308e3b07ac98f
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-10 17:51:38 +0900

    fix/cart-003 : 주송지을 받아오긴 위한 변경

--- lib/features/cart/service/cart_service.dart
+++ lib/features/cart/service/cart_service.dart
@@ -76,15 +76,33 @@ class CartService {
     }
   }
 
-  Future<void> checkoutCart(List<String> cartIds) async {
+  Future<void> checkoutCart({
+    required List<String> cartIds,
+    required String recipient,
+    required String address,
+    required String phone,
+    required String zipCode,
+    String memo = '',
+  }) async {
     try {
-      final body = {'cartIds': cartIds};
+      final body = {
+        "shippingInfo": {
+          "recipient": recipient,
+          "address": address,
+          "phone": phone,
+          "zipCode": zipCode,
+        },
+        "memo": memo,
+        "cartIds": cartIds,
+      };
+
       final response = await _dio.post('/api/cart/checkout', data: body);
 
       if (response.statusCode != 200 && response.statusCode != 201) {
         throw Exception('주문 생성 실패: ${response.statusCode}');
       }
-      print('[주문 생성 성공] $response');
+
+      print('[주문 생성 성공] ${response.data}');
     } catch (e) {
       print('[주문 생성 실패] $e');
       rethrow;
--- lib/features/purchase/screen/purchase_screen.dart
+++ lib/features/purchase/screen/purchase_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemarket/core/theme/app_colors.dart';
+import 'package:cinemarket/features/auth/viewmodel/my_page_viewmodel.dart';
 import 'package:cinemarket/features/cart/service/cart_service.dart';
 import 'package:cinemarket/features/cart/widgets/cart_item_widgets.dart';
 import 'package:cinemarket/features/purchase/widgets/bottom_action_button.dart';
@@ -8,6 +9,7 @@ import 'package:cinemarket/features/purchase/widgets/payment_info_card.dart';
 import 'package:cinemarket/widgets/common_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:toastification/toastification.dart';
 
 class PurchaseScreen extends StatefulWidget {
@@ -22,11 +24,22 @@ class PurchaseScreen extends StatefulWidget {
 class _PurchaseScreen extends State<PurchaseScreen> {
   late List<CartItem> cartItems;
   final CartService _cartService = CartService();
+  final memoController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     cartItems = widget.cartItems;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<MyPageViewModel>().initialize();
+    });
+  }
+
+  @override
+  void dispose() {
+    memoController.dispose();
+    super.dispose();
   }
 
   void increaseQuantity(int index) {
@@ -43,11 +56,18 @@ class _PurchaseScreen extends State<PurchaseScreen> {
     });
   }
 
+
+
   int get totalPrice =>
       cartItems.fold(0, (sum, item) => sum + item.price * item.quantity);
 
   @override
   Widget build(BuildContext context) {
+    final myPageViewModel = context.watch<MyPageViewModel>();
+    final address = myPageViewModel.address1;
+    final zipCode = myPageViewModel.zipCode;
+    final name = myPageViewModel.recipientName;
+
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
@@ -74,26 +94,63 @@ class _PurchaseScreen extends State<PurchaseScreen> {
             );
           }),
           const SizedBox(height: 16),
-          const DeliveryInfoCard(),
+
+          // 배송지 카드 (주소 선택 포함)
+          DeliveryInfoCard(
+            address: address,
+            zipCode: zipCode,
+            onAddressChanged: (newAddress, newZip) {
+              myPageViewModel.setAddress(newAddress, newZip);
+            },
+          ),
+
           const SizedBox(height: 16),
           PaymentInfoCard(items: cartItems),
-          const SizedBox(height: 80), // 공간 확보
+
+          const SizedBox(height: 16),
+          // TextField(
+          //   controller: memoController,
+          //   style: const TextStyle(color: Colors.white),
+          //   decoration: const InputDecoration(
+          //     labelText: '배송 메모 (선택)',
+          //     labelStyle: TextStyle(color: Colors.white),
+          //     filled: true,
+          //     fillColor: Colors.grey,
+          //     border: OutlineInputBorder(),
+          //   ),
+          // ),
+          const SizedBox(height: 80),
         ],
       ),
       bottomNavigationBar: BottomActionButton(
         label: '구매',
-        onPressed: () async{
-          // 구매 완료 토스트 후 마이페이지 이동
+        onPressed: () async {
+          if (address == null || zipCode == null) {
+            CommonToast.show(
+              context: context,
+              message: '배송지를 선택해주세요.',
+              type: ToastificationType.error,
+            );
+            return;
+          }
+
           try {
             final cartIds = cartItems.map((e) => e.cartId).toList();
-            await _cartService.checkoutCart(cartIds);
+            await _cartService.checkoutCart(
+              cartIds: cartIds,
+              recipient: name,
+              address: address,
+              phone: myPageViewModel.safePhone,
+              zipCode: zipCode!,
+              memo: memoController.text,
+            );
             CommonToast.show(
               context: context,
               message: '구매가 완료되었습니다.',
               type: ToastificationType.success,
             );
-            Future.delayed(const Duration(milliseconds: 600), () {
-              context.go('/home'); // 구매후 마이페이지(주문내역) 이동
+            Future.delayed(const Duration(seconds: 1), () {
+              context.go('/home');
             });
           } catch (e) {
             CommonToast.show(
