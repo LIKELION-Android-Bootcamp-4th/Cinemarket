5bbdfcbe5cec98e431ebf88a6a79a32ba6d3ad44
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-11 22:35:17 +0900

    feat/purchase-001

--- lib/features/goods/services/goods_cart_service.dart
+++ lib/features/goods/services/goods_cart_service.dart
@@ -1,6 +1,9 @@
 import 'package:cinemarket/core/network/api_client.dart';
+import 'package:dio/dio.dart';
 
 class GoodsCartService {
+  final Dio dio = ApiClient.dio;
+
   Future<void> addToCart({
     required String productId,
     required int quantity,
@@ -13,8 +16,11 @@ class GoodsCartService {
       'unitPrice': unitPrice,
     });
 
-    if (response.statusCode != 200 || response.data['success'] != true) {
+    if ((response.statusCode != 200 && response.statusCode != 201) || response.data['success'] != true) {
+      print('[장바구니 추가 실패 응답] statusCode: ${response.statusCode}, body: ${response.data}');
       throw Exception('장바구니 추가 실패');
+    } else {
+      print('[장바구니 추가 성공] statusCode: ${response.statusCode}, body: ${response.data}');
     }
   }
 }
