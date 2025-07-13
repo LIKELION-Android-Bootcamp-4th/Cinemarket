887a39214432c109e24552a3b3cbb859ddabafe4
HyunjeLee <cyeclamen07@gmail.com>
2025-07-13 23:55:16 +0900

    fix: f01/f02에서 common_tab_view 사용

--- lib/features/favorite/screen/favorite_screen.dart
+++ lib/features/favorite/screen/favorite_screen.dart
@@ -3,11 +3,14 @@ import 'package:cinemarket/core/theme/app_text_style.dart';
 import 'package:cinemarket/features/favorite/viewmodel/favorite_viewmodel.dart';
 import 'package:cinemarket/widgets/common_gridview.dart';
 import 'package:cinemarket/core/constants/enums/item_type.dart';
+import 'package:cinemarket/widgets/common_tab_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
+
+  static const List<String> _tabTitles = ['굿즈', '영화'];
 
   @override
   Widget build(BuildContext context) {
@@ -23,49 +26,20 @@ class FavoriteScreen extends StatelessWidget {
         final viewModel = context.read<FavoriteViewModel>();
         viewModel.getAllFavoriteGoods();
 
-        return DefaultTabController(
-          length: 2,
-          child: Container(
-            color: AppColors.background,
-            child: Column(
-              children: [
-                const Material(
-                  color: AppColors.widgetBackground,
-                  child: TabBar(
-                    tabs: [
-                      // Tab(child: Text('굿즈', style: TextStyle(fontSize: 20))),
-                      Tab(child: Text('굿즈', style: TextStyle(fontSize: 20))),
-                      Tab(child: Text('영화', style: AppTextStyle.section)),
-                      // todo: 커스텀 색상 사용 시 라벨 적용 안댐
-                      // Tab(child: Text('영화', style: AppTextStyle.headline)),
-                    ],
-                    labelColor: AppColors.textPoint,
-                    unselectedLabelColor: AppColors.textPrimary,
-                    indicatorColor: AppColors.innerWidget,
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: TabBarView(
-                      children: [
-                        CommonGridview(
-                          itemType: ItemType.goods,
-                          // items: dummyGoods,
-                          items: viewModel.favoriteGoods,
-                        ),
-                        CommonGridview(
-                          itemType: ItemType.movie,
-                          // items: dummyMovies,
-                          items: [],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+        return CommonTabView(
+          tabTitles: _tabTitles,
+          tabViews: [
+            CommonGridview(
+              itemType: ItemType.goods,
+              // items: dummyGoods,
+              items: viewModel.favoriteGoods,
             ),
-          ),
+            const CommonGridview(
+              itemType: ItemType.movie,
+              // items: dummyMovies,
+              items: [],
+            ),
+          ],
         );
       },
     );
