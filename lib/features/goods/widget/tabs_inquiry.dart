e63af2a4be13bf77af779cc471c8c3a9ee6102aa
HyunjeLee <cyeclamen07@gmail.com>
2025-07-14 03:13:38 +0900

    fix: 배송&환불, 문의 탭에 기본값 추가

--- lib/features/goods/widget/tabs_inquiry.dart
+++ lib/features/goods/widget/tabs_inquiry.dart
@@ -1,7 +1,11 @@
+import 'package:cinemarket/core/theme/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 List<Widget> getTabsInquiryWidgets() {
   return [
-    // todo
+    const SizedBox(
+      height: 400,
+      child: Center(child: Text('구현 중..', style: AppTextStyle.headline)),
+    ),
   ];
 }
