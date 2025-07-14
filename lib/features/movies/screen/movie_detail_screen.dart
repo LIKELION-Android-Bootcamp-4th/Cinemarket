430d006aaf8d4fc18cdbb8edea338253a52a50cc
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-07-14 14:46:48 +0900

    [fix/M01] 폴더 위치 수정 및 main.dart 내 MultiProvider 에 안 쓰는 ViewModel 제거

--- lib/features/movies/screen/movie_detail_screen.dart
+++ lib/features/movies/screen/movie_detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:cinemarket/core/theme/app_text_style.dart';
-import 'package:cinemarket/features/home/widgets/build_best_goods_grid.dart';
+import 'package:cinemarket/features/movies/widgets/build_best_goods_grid.dart';
 import 'package:cinemarket/features/movies/viewmodel/movie_detail_viewmodel.dart';
 import 'package:cinemarket/features/movies/widgets/cast_grid_view.dart';
 import 'package:cinemarket/features/movies/widgets/movie_detail_header.dart';
@@ -28,98 +28,92 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
   bool isFavorite = false;
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final movieId = int.parse(widget.movieId);
-      final vm = context.read<MovieDetailViewModel>();
-      vm.loadMovieDetail(movieId);
-      vm.loadRecommendedGoods(widget.movieId);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer<MovieDetailViewModel>(
-      builder: (context, vm, child) {
-        if (vm.isLoading) {
-          return const Center(child: CircularProgressIndicator());
-        }
+    return ChangeNotifierProvider<MovieDetailViewModel>(
+      create: (_) => MovieDetailViewModel()
+        ..loadMovieDetail(int.parse(widget.movieId))
+        ..loadRecommendedGoods(widget.movieId),
+      child: Consumer<MovieDetailViewModel>(
+        builder: (context, vm, child) {
+          if (vm.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        final movieDetail = vm.movieDetail;
-        final castList = vm.castList;
+          final movieDetail = vm.movieDetail;
+          final castList = vm.castList;
 
-        return Scaffold(
-          appBar: const CommonAppBar(title: '영화 상세'),
-          backgroundColor: AppColors.background,
-          body: SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MovieDetailHeader(
-                    posterUrl:
-                        'https://image.tmdb.org/t/p/w500${movieDetail!.backdropPath}',
-                    thumbnailUrl:
-                        'https://image.tmdb.org/t/p/w500${movieDetail.posterPath}',
-                    isFavorite: isFavorite,
-                    onFavoriteToggle: () async {
-                      // 로그인 요청  // 하지 않는다면 바로 action 종료
-                      if (!await requireLoginBeforeAction(context)) return;
+          return Scaffold(
+            appBar: const CommonAppBar(title: '영화 상세'),
+            backgroundColor: AppColors.background,
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    MovieDetailHeader(
+                      posterUrl:
+                      'https://image.tmdb.org/t/p/w500${movieDetail!.backdropPath}',
+                      thumbnailUrl:
+                      'https://image.tmdb.org/t/p/w500${movieDetail.posterPath}',
+                      isFavorite: isFavorite,
+                      onFavoriteToggle: () async {
+                        // 로그인 요청  // 하지 않는다면 바로 action 종료
+                        if (!await requireLoginBeforeAction(context)) return;
 
-                      if (await updateFavoriteStatus(movieId: widget.movieId)) {
-                        if (!context.mounted) return;
+                        if (await updateFavoriteStatus(movieId: widget.movieId)) {
+                          if (!context.mounted) return;
 
-                        setState(() => isFavorite = !isFavorite);
-                        CommonToast.show(
-                          context: context,
-                          message: isFavorite ? '찜 추가 완료 !' : '찜 삭제 완료 !',
-                          type: ToastificationType.success,
-                        );
-                      }
-                    },
-                    title: movieDetail.title,
-                    voteAverage: movieDetail.voteAverage,
-                    providers: movieDetail.providers ?? [],
-                  ),
-
-                  MovieInfoRow(
-                    genres: movieDetail.genres,
-                    releaseYear: movieDetail.releaseYear,
-                    runtime: movieDetail.runtime,
-                  ),
-                  const SizedBox(height: 30),
-
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    child: CommonTabView(
-                      tabTitles: const ['줄거리 요약', '굿즈', '출연진'],
-                      tabViews: [
-                        Padding(
-                          padding: EdgeInsets.all(16),
-                          child: SynopsisText(synopsis: movieDetail.overview),
-                        ),
-                        vm.goodsList.isEmpty
-                            ? Center(
-                              child: Text(
-                                '굿즈 정보가 없습니다.',
-                                style: AppTextStyle.body,
-                              ),
-                            )
-                            : buildBestGoodsGrid(
-                              vm.goodsList,
-                              movieDetail.title,
-                            ),
-                        CastGridView(castList: castList),
-                      ],
+                          setState(() => isFavorite = !isFavorite);
+                          CommonToast.show(
+                            context: context,
+                            message: isFavorite ? '찜 추가 완료 !' : '찜 삭제 완료 !',
+                            type: ToastificationType.success,
+                          );
+                        }
+                      },
+                      title: movieDetail.title,
+                      voteAverage: movieDetail.voteAverage,
+                      providers: movieDetail.providers ?? [],
                     ),
-                  ),
-                ],
+
+                    MovieInfoRow(
+                      genres: movieDetail.genres,
+                      releaseYear: movieDetail.releaseYear,
+                      runtime: movieDetail.runtime,
+                    ),
+                    const SizedBox(height: 30),
+
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: CommonTabView(
+                        tabTitles: const ['줄거리 요약', '굿즈', '출연진'],
+                        tabViews: [
+                          Padding(
+                            padding: EdgeInsets.all(16),
+                            child: SynopsisText(synopsis: movieDetail.overview),
+                          ),
+                          vm.goodsList.isEmpty
+                              ? Center(
+                            child: Text(
+                              '굿즈 정보가 없습니다.',
+                              style: AppTextStyle.body,
+                            ),
+                          )
+                              : buildBestGoodsGrid(
+                            vm.goodsList,
+                            movieDetail.title,
+                          ),
+                          CastGridView(castList: castList),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
