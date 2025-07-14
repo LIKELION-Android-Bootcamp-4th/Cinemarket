3fd00675b180afb0e1550d48c25b9dc6a251a152
KIMHAKROK <gkrfhr0039@naver.com>
2025-07-14 22:16:19 +0900

    fix : searchscreen 데이터 연결

--- lib/features/search/model/search_goods_model.dart
+++ lib/features/search/model/search_goods_model.dart
@@ -1,18 +1,38 @@
+import 'package:cinemarket/features/goods/model/review_stats.dart';
+
 class SearchItem {
   final String id;
   final String name;
   final String description;
   final int price;
   final String mainImage;
+  final int stock;
+  final String status;
+  final int viewCount;
+  final int orderCount;
+  final int reviewCount;
+  final String createdAt;
+  final int favoriteCount;
+  final ReviewStats? reviewStats;
   final int? tmdbId;
+  final bool isFavorite;
 
   SearchItem({
     required this.id,
     required this.name,
     required this.description,
     required this.price,
+    required this.stock,
+    required this.status,
+    required this.viewCount,
+    required this.orderCount,
+    required this.reviewCount,
+    required this.createdAt,
     required this.mainImage,
+    required this.favoriteCount,
+    this.reviewStats,
     this.tmdbId,
+    required this.isFavorite,
   });
 
   factory SearchItem.fromJson(Map<String, dynamic> json) {
@@ -22,7 +42,18 @@ class SearchItem {
       name: json['name'] ?? '',
       description: json['description'] ?? '',
       price: json['price'] ?? 0,
+      stock: json['stock'] ?? 0,
+      status: json['status'] ?? 'unknown',
+      favoriteCount: json['favoriteCount'] ?? 0,
+      viewCount: json['viewCount'] ?? 0,
+      orderCount: json['orderCount'] ?? 0,
+      reviewCount: json['reviewCount'] ?? 0,
       mainImage: imageMap['main'] ?? '',
+      createdAt: json['createdAt'] ?? '',
+      reviewStats: json['reviewStats'] != null
+          ? ReviewStats.fromJson(json['reviewStats'])
+          : null,
+      isFavorite: json['isFavorite'] ?? false,
     );
   }
 }
